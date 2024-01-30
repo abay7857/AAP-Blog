591d3ff10fc8d07f89afa98be28c51d79eecd1ac
itszun <malikal.j4@gmail.com>
2024-01-30 21:12:52 +0700

    Home Home

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ use App\Models\User;
 
 Route::get('/', function () {
     return view( 'home', [
-        "title" => "Home",
+        "title" => "Home Home Home",
         'active' => 'home',
     ]);
 });
