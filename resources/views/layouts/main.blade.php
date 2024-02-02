d5eaec1f5ea780d3cce416c250883b87dbae075d
abay7857 <abay7857@gmail.com>
2024-02-02 15:37:43 +0700

    navbar login dan form register

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -9,6 +9,13 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.rtl.min.css" integrity="sha384-nU14brUcp6StFntEOOEBvcJm4huWjB0OcIeQ3fltAfSmuZFrkAif0T+UtNGlKKQv" crossorigin="anonymous">
 
     <title>AAP Blog | {{ $title }}</title>
+
+    {{-- Bootstrap Icons --}}
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+
+    {{-- My Style --}}
+    <link rel="stylesheet" href="/css/style.css">
 </head>
 
 <body>
